572d478aed94e6dad8616613b48100a7046d26f7
Prates <guipratesdsantos@gmail.com>
2023-09-19 16:47:33 -0300

    pokedex desafio dio

--- assets/js/selected.js
+++ assets/js/selected.js
@@ -1,5 +1,6 @@
 const pokeID = location.search.split('=')[1]
 const targetPokemon = document.querySelector('.content-target')
+const barrinha = document.querySelector('.barrinha span')
 
 async function getDetail() {
     const url1 = `https://pokeapi.co/api/v2/pokemon/${pokeID}/`
@@ -54,32 +55,32 @@ async function newInfo() {
                 <h6>HP</h6>
                 <div>
                     <span>${pokemon.hp}</span>
-                    <div class="barrinha"><span class="hp"></span></div>
+                    <div class="barrinha"><span style="width:${pokemon.hp + 10}px;"></span></div>
                 </div>
                 <h6>ATTACK</h6>
                 <div>
                     <span>${pokemon.atack}</span>
-                    <div class="barrinha"><span class="atak"></span></div>
+                    <div class="barrinha"><span style="width:${pokemon.atack + 10}px;"></span></div>
                 </div>
                 <h6>DEFENSE</h6>
                 <div>
                     <span>${pokemon.defense}</span>
-                    <div class="barrinha"><span class="def"></span></div>
+                    <div class="barrinha"><span style="width:${pokemon.defense + 10}px;"></span></div>
                 </div>
                 <h6>SP.ATTACK</h6>
                 <div>
                     <span>${pokemon.atackSp}</span>
-                    <div class="barrinha"><span class="sp-atak"></span></div>
+                    <div class="barrinha"><span style="width:${pokemon.atackSp + 10}px;"></span></div>
                 </div>
                 <h6>SP.DEFENSE</h6>
                 <div>
                     <span>${pokemon.defenseSp}</span>
-                    <div class="barrinha"><span class="sp-def"></span></div>
+                    <div class="barrinha"><span style="width:${pokemon.defenseSp + 10}px;"></span></div>
                 </div>
                 <h6>SPEED</h6>
                 <div>
                     <span>${pokemon.speed}</span>
-                    <div class="barrinha"><span class="speed"></span></div>
+                    <div class="barrinha"><span style="width:${pokemon.speed + 10}px;"></span></div>
                 </div>
             </div>
         </article>
